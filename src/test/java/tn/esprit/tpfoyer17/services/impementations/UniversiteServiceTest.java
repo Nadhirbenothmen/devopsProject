11cc6be6204e1eaa7f39730f5855b9567b1bdb5b
Hadilarfaoui <arfaoui.hadil@esprit.tn>
2024-11-01 10:24:36 +0100

    Commit 10

--- src/test/java/tn/esprit/tpfoyer17/services/impementations/UniversiteServiceTest.java
+++ src/test/java/tn/esprit/tpfoyer17/services/impementations/UniversiteServiceTest.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+//test test
 @ExtendWith(SpringExtension.class)
 class UniversiteServiceTest {
 
