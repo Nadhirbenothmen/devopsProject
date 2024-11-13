e274724b91696ba3120d04a1641b22a8d91d9f15
Hadilarfaoui <arfaoui.hadil@esprit.tn>
2024-11-13 13:09:55 +0100

    Commit 55

--- src/test/java/tn/esprit/tpfoyer17/services/impementations/UniversiteServiceTest.java
+++ src/test/java/tn/esprit/tpfoyer17/services/impementations/UniversiteServiceTest.java
@@ -15,7 +15,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class UniversiteServiceTest {
-
+//*
     @Mock
     private UniversiteRepository universiteRepository;
 
